d64b1c764b8c219a727998f3673fbb612b966c89
jeremiasolivera <jeremiasolivera166@gmail.com>
2023-05-18 14:13:37 -0300

    statics

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
